862f85df1de5fb73f7be6c948e4c68a97416026c
C4rol-sudo <acam5@aluno.ifal.edu.br>
2026-05-08 16:37:16 -0300

    teste4

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// 1. Localiza o botão e a caixa de destino
 const botao = document.getElementById('btn-regras');
 const destino = document.getElementById('container-tabela');
 
-// 2. Cria a função que busca o arquivo
 botao.addEventListener('click', () => {
     
-    // O fetch vai "chamar" o arquivo regras.html
     fetch('programacao.html')
         .then(resposta => {
-            // Se der tudo certo, pega o texto do arquivo
+            
             return resposta.text();
         })
         .then(html => {
-            // Joga o HTML da tabela dentro da nossa div
+            
             destino.innerHTML = html;
         })
         .catch(erro => {
